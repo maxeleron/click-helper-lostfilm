d741e24438d178fa4c4173e302de3936b39d69d4
Max Ponomarenko <maxeleron@gmail.com>
2020-12-10 17:04:58 +0200

    0.1 version - working

---
+++ click_close.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Selector of the 1080 link, I`ve got it from 'Inspect Element'
+document.querySelector("div.inner-box--list > div:nth-child(2) > div.inner-box--link.main > a").click();
+
+//trick move to close insearch.site window
+setTimeout( ()=>{window.close()}, 100);
+
+
+
